ada27390a4187ccf842e0a818b65fbd62810e0f0
Shraya05 <163320293+Shraya05@users.noreply.github.com>
2024-12-13 00:10:18 +0530

    [3] [Time Beats: 87.42%] [Memory Beats: 50.87%] - LeetPush

--- 3-Longest-Substring-Without-Repeating-Characters.java
+++ 3-Longest-Substring-Without-Repeating-Characters.java
@@ -1,20 +1,19 @@
 class Solution {
     public int lengthOfLongestSubstring(String s) {
-       int n = s.length();
-        int maxLength = 0;
-        Map<Character, Integer> charMap = new HashMap<>();
+          Map<Character, Integer> map = new HashMap<>();
         int left = 0;
-        
-        for (int right = 0; right < n; right++) {
-            if (!charMap.containsKey(s.charAt(right)) || charMap.get(s.charAt(right)) < left) {
-                charMap.put(s.charAt(right), right);
-                maxLength = Math.max(maxLength, right - left + 1);
-            } else {
-                left = charMap.get(s.charAt(right)) + 1;
-                charMap.put(s.charAt(right), right);
+        int max = 0;
+
+        for (int right = 0; right < s.length(); right++) {
+            char c = s.charAt(right);
+
+            if (map.containsKey(c) && map.get(c) >= left) {
+                left = map.get(c) + 1; 
             }
+            map.put(c, right);
+            max = Math.max(max, right - left + 1);
         }
-        
-        return maxLength;
+
+        return max;
     }
 }
