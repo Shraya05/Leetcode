3c99d95aa89f17c88ece049474994af8d386ca93
Shraya05 <163320293+Shraya05@users.noreply.github.com>
2024-12-14 19:40:55 +0530

    [122] [Time Beats: 92.23%] [Memory Beats: 41.48%] - LeetPush

--- 122-Best-Time-to-Buy-and-Sell-Stock-II.java
+++ 122-Best-Time-to-Buy-and-Sell-Stock-II.java
@@ -1,14 +1,13 @@
 class Solution {
     public int maxProfit(int[] prices) {
-        int hold = -Integer.MAX_VALUE, notHold = 0;
-        
-        for( int stockPrice : prices ){
-            
-            int prevHold = hold, prevNotHold = notHold;
-            hold = Math.max(prevHold, prevNotHold - stockPrice);
-            notHold = Math.max(prevNotHold, prevHold + stockPrice);
-            
+        int maxprofit=0;
+        for(int i=1;i<prices.length;i++)
+        {
+            if(prices[i]>prices[i-1])
+            {
+                maxprofit+=prices[i]-prices[i-1];
+            }
         }
-        return notHold;
+        return maxprofit;
     }
 }
