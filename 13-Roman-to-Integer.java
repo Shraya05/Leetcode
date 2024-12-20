ca9429d2bc378f55444684e1be5c548853f5651a
Shraya05 <163320293+Shraya05@users.noreply.github.com>
2024-12-20 23:53:50 +0530

    [13] [Time Beats: 29.84%] [Memory Beats: 98.86%] - LeetPush

--- 13-Roman-to-Integer.java
+++ 13-Roman-to-Integer.java
@@ -1,30 +1,25 @@
 class Solution {
     public int romanToInt(String s) {
-         HashMap<Character, Integer> map = new HashMap<>();
-        map.put('I', 1);
-        map.put('V', 5);
-        map.put('X', 10);
-        map.put('L', 50);
-        map.put('C', 100);
-        map.put('D', 500);
-        map.put('M', 1000);
-
-        int result = 0;
-        int prevValue = 0;
-       
-        for (int i = s.length() - 1; i >= 0; i--) {
-            int value = map.get(s.charAt(i));
-
-
-            if (value < prevValue) {
-                result -= value;
-            } else {
-                result += value;
-            }
-            prevValue = value;
-        }
-
-        return result;
-    
+        Map<Character,Integer> map= new HashMap<>();
+        map.put('I',1);
+         map.put('V',5);
+         map.put('X',10);
+         map.put('L',50);
+         map.put('C',100);
+         map.put('D',500);
+         map.put('M',1000);
+         int res=0;
+         for(int i=0;i<s.length();i++)
+         {
+              if(i<s.length()-1 && map.get(s.charAt(i)) < map.get(s.charAt(i+1)))
+              {
+                res-=map.get(s.charAt(i));
+              }
+              else
+              {
+                res+=map.get(s.charAt(i));
+              }
+         }
+      return res;
     }
 }
